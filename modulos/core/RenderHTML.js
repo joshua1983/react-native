9c5ef2ed70055892812d572eaa57e0497ed83e83
Josue <josue.barrios@gmail.com>
2018-07-24 22:59:33 -0500

    agregado componente plantilla dos botones

--- modulos/core/RenderHTML.js
+++ modulos/core/RenderHTML.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 import HTML from 'react-native-render-html';
+import { Button, Grid, Col, Row, Content, CardItem, Card, Body } from 'native-base';
+import  DosBotones  from './templates/DosBotones';
 
-const deviceHeight = Dimensions.get('window').height;
-const deviceWidth = Dimensions.get('window').width;
+//const deviceHeight = Dimensions.get('window').height;
+//const deviceWidth = Dimensions.get('window').width;
 
 export default class RenderHTML extends React.Component {
 
@@ -20,16 +22,18 @@ export default class RenderHTML extends React.Component {
         pagina: props.Pagina
 
       }
+      this.dosBotones = React.createRef();
     }
   
     componentDidMount(){
-      return fetch('http://desarrollo.yesynergy.com/admin/index.php/mobile/getPagina/1/1')
+      return fetch('http://admin.yesynergy.com/index.php/mobile/getPagina/1/1')
         .then((response) => response.json())
         .then((responseJson) => {
   
           this.setState({
             cargando: false,
-            html: responseJson.Pagina.html,
+            html: responseJson[0].html,
+            tipo: responseJson[0].tipo
           }, function(){
   
           });
@@ -38,6 +42,10 @@ export default class RenderHTML extends React.Component {
         .catch((error) =>{
           console.error(error);
         });
+    }
+
+    _siguiente = () => {
+      this.dosBotones._guardar();
     }
   
   
@@ -52,22 +60,45 @@ export default class RenderHTML extends React.Component {
   
   
       return (
-        <ScrollView>
+        <ScrollView style={estilos.container}>
           <HTML 
             html={this.state.html}
             /> 
+
+          <Content>
+            <Card>
+              <CardItem>
+                <Body>
+                  { this.state.tipo == "1" && 
+                  <DosBotones 
+                    ref = { instancia => { this.dosBotones = instancia }}
+                  />
+                  }
+                </Body>
+                </CardItem>
+            </Card>
+            <Card>
+              <CardItem>
+                <Body>
+                  <Button iconRight rounded success onPress={this._siguiente} > 
+                    <Text style={estilos.textButton} >Siguiente </Text>
+                  </Button>
+                </Body>
+              </CardItem>
+            </Card>
+          </Content>
+
         </ScrollView>
          
       );
     }
   }
   
-  const styles = StyleSheet.create({
+  const estilos = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      width: deviceWidth,
-      height: deviceHeight
+      padding: 5
     },
+    textButton: {
+      padding: 10
+    }
   });
---
+++ modulos/core/templates/DosBotones.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Dimensions, Platform, ToastAndroid } from 'react-native';
+import { Grid, Col, Button, Toast } from 'native-base';
+
+
+class DosBotones extends React.Component {
+
+    state = {
+        opcion1text: 'Opcion 1',
+        opcion1valor: '0',
+        opcion2text: 'Opcion 2',
+        opcion2valor: '0',
+        opcionSeleccionada: '0'
+    }
+
+    constructor(props){
+      super(props);    
+
+    }
+
+    _selOpcion1(){
+        this.setState({
+            opcionSeleccionada: this.state.opcion1valor
+        });
+    }
+    _selOpcion2(){
+        this.setState({
+            opcionSeleccionada: this.state.opcion2valor
+        });
+    }
+
+    _guardar(){
+        this._informar();
+    }
+
+    _informar(){
+        if (Platform.OS == 'android'){
+            ToastAndroid.show('Datos guardados!', ToastAndroid.SHORT);
+        }else{
+            Alert.alert(
+                'Plataforma',
+                'Datos guardados',
+                [
+                  {text: 'Ok'}
+                ],
+                { cancelable: false }
+              )
+        }
+
+    }
+
+    render() {
+      return (
+
+        <Grid>
+            <Col>
+                <Button info>
+                    <Text style={esDosBotones.textButton}> {this.state.opcion1text}</Text>
+                </Button>
+            </Col>
+            <Col>
+                <Button warning>
+                    <Text style={esDosBotones.textButton}> {this.state.opcion2text}</Text>
+                </Button>
+            </Col>
+        </Grid>
+        
+         
+      );
+    }
+  }
+
+  const esDosBotones = StyleSheet.create({
+    container: {
+      padding: 5
+    },
+    textButton: {
+      padding: 20
+    }
+  });
+
+
+  export default DosBotones;
